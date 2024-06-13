dc67d45425e2067b7146302299c9f3dab2c3f442
Audreylopez22 <112126647+Audreylopez22@users.noreply.github.com>
2024-06-13 08:48:31 -0500

    payment reminders and login secrets  (#3)
    * first version of pdf download
    
    * modify for requirements.txt
    
    * changes for folder files for tmp
    
    * changes for folder files for tmp test 1
    
    * changes for folder files for tmp test 2
    
    * changes in documentation and date in PDF
    
    * changes in documentation and date in PDF test
    
    * changes in documentation and date in PDF test 2
    
    * changes in PDF Download  test 1
    
    * changes in PDF Download  test 2
    
    * changes in PDF Download test 3
    
    * changes in PDF Download test 4
    
    * changes in PDF Download test 5
    
    * changes in PDF Download test 6
    
    * changes in PDF Download test 7
    
    * changes in PDF Download test 7
    
    * changes in PDF Download test 8
    
    * changes in PDF Download test 9
    
    * Enable Docker replicability and adjust config.ini
    
    * The readme was modified to add Docker instructions and the Python version was added to the documentation.
    
    * Enable Docker replicability and adjust config.ini
    
    * payment reminders and login secrets

--- 🔐_Login.py
+++ 🔐_Login.py
@@ -2,6 +2,7 @@ import streamlit as st
 import yaml
 from yaml.loader import SafeLoader
 import streamlit_authenticator as stauth
+from datetime import datetime
 
 st.set_page_config(page_title="Login", page_icon="👋", layout="wide")
 
@@ -11,26 +12,37 @@ with open("./config.yaml", "r") as file:
 for i, username in enumerate(config["credentials"]["usernames"]):
     config["credentials"]["passwords"][i] = st.secrets.paswords[username]
 
+now = datetime.now()
+now_day = now.day
+
+if now_day >= 1 and now_day < 6:
+    st.warning("Remember to pay your membership the first 5 days of the month.")
+
 
 def main():
-    authenticator = stauth.Authenticate(
-        config["credentials"]["names"],
-        config["credentials"]["usernames"],
-        config["credentials"]["passwords"],
-        config["cookie"]["name"],
-        config["cookie"]["key"],
-        config["cookie"]["expiry_days"],
-    )
+    if st.secrets.subscription.active:
+        authenticator = stauth.Authenticate(
+            config["credentials"]["names"],
+            config["credentials"]["usernames"],
+            config["credentials"]["passwords"],
+            config["cookie"]["name"],
+            config["cookie"]["key"],
+            config["cookie"]["expiry_days"],
+        )
 
-    name, authentication_status, username = authenticator.login("Login", "main")
+        name, authentication_status, username = authenticator.login("Login", "main")
 
-    if st.session_state["authentication_status"]:
-        st.write(f'Welcome *{st.session_state["name"]}*')
-        authenticator.logout("Logout", "main")
-    elif st.session_state["authentication_status"] is False:
-        st.error("Username/password is incorrect")
-    elif st.session_state["authentication_status"] is None:
-        st.warning("Please enter your username and password")
+        if st.session_state["authentication_status"]:
+            st.write(f'Welcome *{st.session_state["name"]}*')
+            authenticator.logout("Logout", "main")
+        elif st.session_state["authentication_status"] is False:
+            st.error("Username/password is incorrect")
+        elif st.session_state["authentication_status"] is None:
+            st.warning("Please enter your username and password")
+    else:
+        st.warning(
+            "Your subscription is inactive. Please make the monthly payment to access the application."
+        )
 
 
 if __name__ == "__main__":
